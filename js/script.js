2c5f3c1ed87743750a005979a5f146575bed98e7
Alberto PIneda Orozco <ing.firemann@mail.com>
2019-07-05 17:13:59 -0500

    Borra ahora lo que el usuario digite en la caja de texto

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 function agregarelemento(){
     var micajadetexto = document.getElementById("idele");
-   
-    if (micajadetexto.value!=''){
-        
-        var ul = document.getElementById("miul");
+      
+    var ul = document.getElementById("miul");
     
-        var li = document.createElement("li");
-        var texto_li = document.createTextNode(micajadetexto.value);
+    var li = document.createElement("li");
+    var texto_li = document.createTextNode(micajadetexto.value);
+    micajadetexto.value = '';
     
-        li.appendChild(texto_li);
-        ul.appendChild(li);
-    }else{
-        alert('Por favor digite un elemento.');
-        
-    }
-   
-
+    li.appendChild(texto_li);
+    ul.appendChild(li);
 }
